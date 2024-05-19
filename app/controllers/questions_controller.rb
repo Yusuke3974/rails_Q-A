4c92f243d26da6d6e37eacaa450ab9fe872347e0
yusuke <ritchie.deep.purple@gmail.com>
2024-05-19 16:21:52 +0900

    initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  # 質問一覧画面
+  def index
+  end
+
+  # 質問詳細画面
+  def show
+  end
+
+  # 質問の作成
+  def new
+  end
+
+  # 質問の編集
+  def edit
+  end
+
+  # 質問の編集
+  def update
+  end
+
+  # 質問の削除
+  def destroy
+  end
+end
